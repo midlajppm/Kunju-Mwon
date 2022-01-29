3d707fddbffb7d95f7b0259994561ddbec71659d
midlajppm <94232701+midlajppm@users.noreply.github.com>
2022-01-29 09:24:40 +0530

    Add files via upload

---
+++ plugins/git.js
@@ -0,0 +1,17 @@
+const Asena = require('../events');
+const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
+const axios = require('axios');
+
+const Language = require('../language');
+const Lang = Language.getString('wallpaper');
+
+Asena.addCommand({pattern: 'git', fromMe: false, desc: "Gives github link of the bot"}, (async (message, match) => {
+
+    var skl = await axios.get("https://images.wallpapersden.com/image/download/pokemon-detective-pikachu-4k_a2lrZmWUmZqaraWkpJRsZ2WtbGdl.jpg", { responseType: 'arraybuffer' })
+
+    await message.sendMessage(Buffer(skl.data), MessageType.image, {mimetype: Mimetype.png, caption: `*Kᴜɴᴊᴜ Ꮇᴡᴏɴ Created By Midlaj*
+
+*Conatct : bit.ly/ContactMidlaj*
+`}) 
+
+}));
